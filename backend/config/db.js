64f6fea457fa45dfe3aa2c7dc55dd14730a47b54
Ritesh-Git-Hub <toomritesh@gmail.com>
2025-08-15 17:18:27 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,3 @@
-// // db.js
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(
-//       'mongodb+srv://mernUser:mernUser@cluster0.nipxf6y.mongodb.net/mernDB?retryWrites=true&w=majority&appName=Cluster0',
-//       {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//       }
-//     );
-//     console.log('✅ MongoDB connected');
-//   } catch (err) {
-//     console.error('❌ Connection error:', err);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
